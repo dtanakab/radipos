c59956796cc13734d4d1e453c7c59151dc43ac39
dtanakab <karugamo.highkick@gmail.com>
2020-01-15 18:11:08 +0900

    CircleCI関連の設定ファイルを追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 server "radipos.site", user: "dtanakab", roles: %w{app web db}
 
 set :ssh_options, {
-  keys: %w(~/.ssh/id_rsa),
+  keys: %w(~/.ssh/id_rsa_3a93b4ea710ae0b5c2731e3a97d3275f),
   port: 59191,
   forward_agent: true,
   auth_methods: %w(publickey)
