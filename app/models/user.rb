fecb9a94713f23669156493aae1176f2b9cbaf08
dtanakab <karugamo.highkick@gmail.com>
2020-01-29 16:50:44 +0900

    テストファイルを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   def age
-    birthday = Date.new(self.birth_year.to_i, self.birth_month.to_i, self.birth_day.to_i)
-    age = (Date.today.strftime("%Y%m%d").to_i - birthday.strftime("%Y%m%d").to_i) / 10000
-    age.to_s
+    if self.birth_year && self.birth_month && self.birth_day
+      birthday = Date.new(self.birth_year.to_i, self.birth_month.to_i, self.birth_day.to_i)
+      age = (Date.today.strftime("%Y%m%d").to_i - birthday.strftime("%Y%m%d").to_i) / 10000
+      age.to_s
+    end
   end
 end
