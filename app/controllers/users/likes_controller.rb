4ba782eb34a415e722ee0fbf89e46411fe67a585
dtanakab <karugamo.highkick@gmail.com>
2020-02-15 18:42:07 +0900

    スタイルシートを分割

--- app/controllers/users/likes_controller.rb
+++ app/controllers/users/likes_controller.rb
@@ -3,10 +3,10 @@
 class Users::LikesController < ApplicationController
   before_action :authenticate_user!
   def index
-    like_programs = []
+    @like_programs_all = []
     current_user.likes.each do | like |
-      like_programs << Program.find(like.program_id)
+      @like_programs_all << Program.find(like.program_id)
     end
-    @like_programs = Kaminari.paginate_array(like_programs).page(params[:page])
+    @like_programs = Kaminari.paginate_array(@like_programs_all).page(params[:page])
   end
 end
